686d8a3b031313833c2edb12d5df4fb78180ce22
bmanek <41875460+bmanek@users.noreply.github.com>
2019-02-22 16:39:29 -0500

    finished with all tests passing

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
+    self[-1] == "?"
 
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    ugh = self.split(%r{[.!?]+\s})
+    ugh.length
   end
+
+
 end
